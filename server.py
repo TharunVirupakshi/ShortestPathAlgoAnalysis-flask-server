6488d9d194857446df56556009c1b88bd205b1fd
TharunVirupakshi <tharunvrrvc184@gmail.com>
2024-02-28 19:54:21 +0530

    Updated functionalities

--- server.py
+++ server.py
@@ -12,7 +12,8 @@ CORS(app, origins=["*"]) #Put your localhost
 
 
 def generate_random_graph(num_nodes, k=5, p=0.1):
-    G = nx.connected_watts_strogatz_graph(num_nodes, k, p)
+
+    G = nx.connected_watts_strogatz_graph(max(num_nodes, 30), k, p)
 
     # Assign labels and groups to nodes
     for node_id, node_data in G.nodes(data=True):
@@ -24,11 +25,6 @@ def generate_random_graph(num_nodes, k=5, p=0.1):
         G[edge[0]][edge[1]]['weight'] = random.randint(1, 20)
 
     return G
-# Generate a random graph with 10 nodes
-
-RandG = None
-
-
 
 
 
@@ -40,8 +36,12 @@ def welcome():
 
 @app.route('/get_rand_graph', methods=['GET'])
 def get_rand_graph():
-    global RandG
-    RandG = generate_random_graph(150)
+
+
+    num_nodes = request.args.get('numNodes', default=150, type=int)
+
+    RandG = generate_random_graph(num_nodes)
+    print("RandG: ", RandG)
     # Prepare the graph data in the specified format (without "type")
     RandData = {
     "directed": False,
@@ -75,30 +75,43 @@ def get_rand_graph():
 
 
 
-@app.route('/get_metrics', methods=['GET'])
+def convertToNxGraph(graph_data):
+
+    # Get JSON data from the client
+    json_data = graph_data
+
+    # Parse JSON data into NetworkX graph
+    parsed_graph = nx.Graph()
+
+    # Add nodes
+    for node in json_data["nodes"]:
+        parsed_graph.add_node(node["id"], label=node["label"], group=node.get("group", None))
+
+    # Add edges
+    for link in json_data["links"]:
+        parsed_graph.add_edge(link["source"], link["target"], weight=link["weight"])
+
+    # # Set RandG to the parsed graph
+    # RandG = parsed_graph
+    print(parsed_graph)
+    
+
+    return parsed_graph
+
+
+
+@app.route('/get_metrics', methods=['POST'])
 def get_metrics():
-    degree_centrality = nx.degree_centrality(G)
-    betweenness_centrality = nx.betweenness_centrality(G)
-    closeness_centrality = nx.closeness_centrality(G)
-    clustering_coefficient = nx.clustering(G)
-    diameter = nx.diameter(G)
 
-    metrics = {
-        'degree_centrality': degree_centrality,
-        'betweenness_centrality': betweenness_centrality,
-        'closeness_centrality': closeness_centrality,
-        'clustering_coefficient': clustering_coefficient,
-        'diameter': diameter
-    }
-    return (metrics)
+    # Parse the request data
+    data = request.get_json()
+    graph = convertToNxGraph(data['graph'])
 
-@app.route('/get_rand_metrics', methods=['GET'])
-def get_rand_metrics():
-    degree_centrality = nx.degree_centrality(RandG)
-    betweenness_centrality = nx.betweenness_centrality(RandG)
-    closeness_centrality = nx.closeness_centrality(RandG)
-    clustering_coefficient = nx.clustering(RandG)
-    diameter = nx.diameter(RandG)
+    degree_centrality = nx.degree_centrality(graph)
+    betweenness_centrality = nx.betweenness_centrality(graph)
+    closeness_centrality = nx.closeness_centrality(graph)
+    clustering_coefficient = nx.clustering(graph)
+    diameter = nx.diameter(graph)
 
     metrics = {
         'degree_centrality': degree_centrality,
@@ -112,44 +125,43 @@ def get_rand_metrics():
 @app.route('/find_shortest_path', methods=['POST'])
 def find_shortest_path():
 
-    #Not making this global can result in incorrect data when finding the shrotest path
-    global RandG 
-   
     # Parse the request data
     data = request.get_json()
+    graph = convertToNxGraph(data['graph'])
     source = data['source']
     target = data['target']
     algorithm = data['algorithm']
+    
   # Default to Dijkstra's algorithm
 
     # Ensure that source and target nodes exist in the RandG graph
-    if source not in RandG.nodes or target not in RandG.nodes:
+    if source not in graph.nodes or target not in graph.nodes:
         return jsonify({'error': 'Source or target node not found in the graph'})
 
     # Find the shortest path based on the selected algorithm
     if algorithm == 'dijkstra':
-        path = nx.shortest_path(RandG, source, target)
+        path = nx.shortest_path(graph, source, target)
         print("Dijkstra Algo executed...")
     elif algorithm == 'dijkstraW':
-        path = nx.shortest_path(RandG, source, target, weight='weight')
+        path = nx.shortest_path(graph, source, target, weight='weight')
         print("Dijkstra (WEIGHTED) Algo executed...")
     elif algorithm == 'bellman-ford':
-        path = nx.shortest_path(RandG, source, target, method="bellman-ford")
+        path = nx.shortest_path(graph, source, target, method="bellman-ford")
         print("Bellman-Ford Algo executed...")
     elif algorithm == 'bellman-fordW':
-        path = nx.shortest_path(RandG, source, target, weight='weight',method="bellman-ford")
+        path = nx.shortest_path(graph, source, target, weight='weight',method="bellman-ford")
         print("Bellman-Ford (WEIGHTED) Algo executed...")
     elif algorithm == 'astar':
-        path = a_star(RandG, source, target)
+        path = a_star(graph, source, target)
         print("A* Algo executed...")
     elif algorithm == 'astarW':
-        path = a_starW(RandG, source, target)
+        path = a_starW(graph, source, target)
         print("A* (WEIGHTED) Algo executed...")
       
     else:
         return jsonify({'error': 'Invalid algorithm'}), 400
     print(path)
-    list(RandG.edges)
+    list(graph.edges)
     return jsonify({'shortest_path': path})
 
 # Function to calculate Euclidean distance between two nodes
